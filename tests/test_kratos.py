6e966cc002999c6b6b66df899581a82b34379003
vryy <hgbk2008@gmail.com>
2019-08-23 15:50:42 +0200

    add a small test to ensure the application is loaded correclty

---
+++ tests/test_kratos.py
@@ -0,0 +1,7 @@
+import KratosMultiphysics
+import KratosMultiphysics.StructuralApplication
+import KratosMultiphysics.BRepApplication
+import KratosMultiphysics.FiniteCellApplication
+import KratosMultiphysics.FiniteCellStructuralApplication
+print("Kratos is loaded successfully")
+
